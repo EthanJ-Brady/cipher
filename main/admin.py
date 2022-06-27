bb61e8baed68785d9f7145379c21eefc7cfbb5cd
EthanJ-Brady <39889225+EthanJ-Brady@users.noreply.github.com>
2022-06-26 19:54:57 -0600

    Converted to database and cleaned up code throughout

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Deck, Source, CodenameCard
 
 # Register your models here.
+admin.site.register(Deck)
+admin.site.register(Source)
+admin.site.register(CodenameCard)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Deck(models.Model):
+    deck_title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.deck_title
+
+class Source(models.Model):
+    source_title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.source_title
+
+class CodenameCard(models.Model):
+    card_title = models.CharField(max_length=30)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name="Cards", null=True)
+    source = models.ManyToManyField(Source, blank=True, related_name="Cards")
+
+    def __str__(self):
+        return self.card_title
